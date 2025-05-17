0cd7fa8c4298b3bf55f072ccbe591753c14e67ce
Julio Oscar <145580879+Xulio1337@users.noreply.github.com>
2025-05-17 18:45:10 -0300

    Update AnimacaoOlhos.cpp

--- Docs/Code/AnimacaoOlhos.cpp
+++ Docs/Code/AnimacaoOlhos.cpp
@@ -13,12 +13,11 @@ AnimacaoOlhos::AnimacaoOlhos(Adafruit_ST7789 &display) : _display(display) {
 }
 
 void AnimacaoOlhos::desenharOlhos() {
-    _display.clearDisplay();
-    _display.fillCircle(CentroOlhoX1, CentroOlhoY1, RaioOcular, WHITE);
-    _display.fillCircle(CentroOlhoX2, CentroOlhoY2, RaioOcular, WHITE);
-    _display.fillCircle(PupilaX1, PupilaY1, TamanhoPupila, BLACK);
-    _display.fillCircle(PupilaX2, PupilaY2, TamanhoPupila, BLACK);
-    _display.display();
+    _display.fillScreen(ST77XX_BLACK);
+    _display.fillCircle(CentroOlhoX1, CentroOlhoY1, RaioOcular, ST77XX_WHITE);
+    _display.fillCircle(CentroOlhoX2, CentroOlhoY2, RaioOcular, ST77XX_WHITE);
+    _display.fillCircle(PupilaX1, PupilaY1, TamanhoPupila, ST77XX_BLACK);
+    _display.fillCircle(PupilaX2, PupilaY2, TamanhoPupila, ST77XX_BLACK);
 }
 
 void AnimacaoOlhos::MoverPupilasParaAngulo(int angulo) {
@@ -47,12 +46,11 @@ void AnimacaoOlhos::MoverPupilasParaAngulo(int angulo) {
     RestringirPosicaoPupila(CentroOlhoX1, CentroOlhoY1, PupilaX1, PupilaY1);
     RestringirPosicaoPupila(CentroOlhoX2, CentroOlhoY2, PupilaX2, PupilaY2);
 
-    _display.clearDisplay();
-    _display.fillCircle(CentroOlhoX1, CentroOlhoY1, RaioOcular, WHITE);
-    _display.fillCircle(CentroOlhoX2, CentroOlhoY2, RaioOcular, WHITE);
-    _display.fillCircle(PupilaX1, PupilaY1, TamanhoPupila, BLACK);
-    _display.fillCircle(PupilaX2, PupilaY2, TamanhoPupila, BLACK);
-    _display.display();
+    _display.fillScreen(ST77XX_BLACK);
+    _display.fillCircle(CentroOlhoX1, CentroOlhoY1, RaioOcular, ST77XX_WHITE);
+    _display.fillCircle(CentroOlhoX2, CentroOlhoY2, RaioOcular, ST77XX_WHITE);
+    _display.fillCircle(PupilaX1, PupilaY1, TamanhoPupila, ST77XX_BLACK);
+    _display.fillCircle(PupilaX2, PupilaY2, TamanhoPupila, ST77XX_BLACK);
 }
 
 void AnimacaoOlhos::RestringirPosicaoPupila(int CentroOlhoX, int CentroOlhoY, int &PupilaX, int &PupilaY) {
@@ -65,38 +63,33 @@ void AnimacaoOlhos::RestringirPosicaoPupila(int CentroOlhoX, int CentroOlhoY, in
 void AnimacaoOlhos::piscar() {
     for (int i = 1; i <= 32; i++) {
         if (i >= 16 && i <= 48) continue;
-        _display.drawLine(0, i - 1, _display.width() - 1, i - 1, BLACK);
-        _display.drawLine(0, _display.height() - i, _display.width() - 1, _display.height() - i, BLACK);
+        _display.drawLine(0, i - 1, _display.width() - 1, i - 1, ST77XX_BLACK);
+        _display.drawLine(0, _display.height() - i, _display.width() - 1, _display.height() - i, ST77XX_BLACK);
     }
-    _display.display();
     delay(5);
 
     for (int i = 1; i <= 32; i++) {
         if (i >= 25 && i <= 39) continue;
-        _display.drawLine(0, i - 1, _display.width() - 1, i - 1, BLACK);
-        _display.drawLine(0, _display.height() - i, _display.width() - 1, _display.height() - i, BLACK);
+        _display.drawLine(0, i - 1, _display.width() - 1, i - 1, ST77XX_BLACK);
+        _display.drawLine(0, _display.height() - i, _display.width() - 1, _display.height() - i, ST77XX_BLACK);
     }
-    _display.display();
     delay(5);
 
     for (int i = 1; i <= 32; i++) {
         if (i >= 32 && i <= 32) continue;
-        _display.drawLine(0, i - 1, _display.width() - 1, i - 1, BLACK);
-        _display.drawLine(0, _display.height() - i, _display.width() - 1, _display.height() - i, BLACK);
+        _display.drawLine(0, i - 1, _display.width() - 1, i - 1, ST77XX_BLACK);
+        _display.drawLine(0, _display.height() - i, _display.width() - 1, _display.height() - i, ST77XX_BLACK);
     }
-    _display.display();
     delay(5);
 
-    _display.clearDisplay();
-    _display.display();
+    _display.fillScreen(ST77XX_BLACK);
     delay(5);
 }
 
 void AnimacaoOlhos::expressaoCansado() {
     for (int i = 0; i <= 26; i++) {
-        _display.drawLine(0, i, _display.width() - 1, i, BLACK);
+        _display.drawLine(0, i, _display.width() - 1, i, ST77XX_BLACK);
     }
-    _display.display();
 }
 
 void AnimacaoOlhos::expressaoTriste() {
@@ -104,7 +97,7 @@ void AnimacaoOlhos::expressaoTriste() {
     int x2 = _display.width() / 2, y2 = 0;
     int x3 = 0, y3 = _display.height() / 2;
 
-    _display.fillTriangle(x1, y1, x2, y2, x3, y3, BLACK);
+    _display.fillTriangle(x1, y1, x2, y2, x3, y3, ST77XX_BLACK);
 
     int x4 = _display.width() - 1, y4 = 0;
     int x5 = _display.width() / 2, y5 = 0;
@@ -119,12 +112,11 @@ void AnimacaoOlhos::expressaoTriste() {
         for (int j = -espessura; j <= espessura; j++) {
             int x = x_centro + raio * cos(i * PI / 180);
             int y = y_centro + raio * sin(i * PI / 180);
-            _display.drawPixel(x, y, WHITE);
+            _display.drawPixel(x, y, ST77XX_WHITE);
         }
     }
 
-    _display.fillTriangle(x4, y4, x5, y5, x6, y6, BLACK);
-    _display.display();
+    _display.fillTriangle(x4, y4, x5, y5, x6, y6, ST77XX_BLACK);
 }
 
 void AnimacaoOlhos::expressaoBase() {
@@ -138,24 +130,22 @@ void AnimacaoOlhos::expressaoBase() {
     PupilaX2 = CentroOlhoX2;
     PupilaY2 = CentroOlhoY2;
 
-    _display.clearDisplay();
-    _display.fillCircle(CentroOlhoX1, CentroOlhoY1, RaioOcular, WHITE);
-    _display.fillCircle(CentroOlhoX2, CentroOlhoY2, RaioOcular, WHITE);
-    _display.fillCircle(PupilaX1, PupilaY1, TamanhoPupila, BLACK);
-    _display.fillCircle(PupilaX2, PupilaY2, TamanhoPupila, BLACK);
-    _display.display();
+    _display.fillScreen(ST77XX_BLACK);
+    _display.fillCircle(CentroOlhoX1, CentroOlhoY1, RaioOcular, ST77XX_WHITE);
+    _display.fillCircle(CentroOlhoX2, CentroOlhoY2, RaioOcular, ST77XX_WHITE);
+    _display.fillCircle(PupilaX1, PupilaY1, TamanhoPupila, ST77XX_BLACK);
+    _display.fillCircle(PupilaX2, PupilaY2, TamanhoPupila, ST77XX_BLACK);
 }
 
 void AnimacaoOlhos::expressaoDesconfiado() {
     for (int i = 0; i <= 26; i++) {
-        _display.drawLine(0, i, _display.width() - 1, i, BLACK);
+        _display.drawLine(0, i, _display.width() - 1, i, ST77XX_BLACK);
     }
     int x1 = 0, y1 = _display.height() - 1;
     int x2 = _display.width() - 1, y2 = _display.height() - 1;
     int x3 = _display.width() / 2, y3 = _display.height() / 2;
 
-    _display.fillTriangle(x1, y1, x2, y2, x3, y3, BLACK);
-    _display.display();
+    _display.fillTriangle(x1, y1, x2, y2, x3, y3, ST77XX_BLACK);
 }
 
 void AnimacaoOlhos::expressaoFeliz() {
@@ -163,12 +153,12 @@ void AnimacaoOlhos::expressaoFeliz() {
     int y1 = _display.height() - 1;
     int raio = 45;
 
-    _display.fillCircle(x1, y1, raio, BLACK);
+    _display.fillCircle(x1, y1, raio, ST77XX_BLACK);
 
     int x2 = _display.width() / 1.3;
     int y2 = _display.height() - 1;
 
-    _display.fillCircle(x2, y2, raio, BLACK);
+    _display.fillCircle(x2, y2, raio, ST77XX_BLACK);
 
     int x_centro1 = _display.width() / 2;
     int y_centro1 = _display.height() - 20;
@@ -179,8 +169,7 @@ void AnimacaoOlhos::expressaoFeliz() {
         for (int j = -espessura; j <= espessura; j++) {
             int x = x_centro1 + (raio2 + j) * cos(i * PI / 180);
             int y = y_centro1 + (raio2 + j) * sin(i * PI / 180);
-            _display.drawPixel(x, y, WHITE);
+            _display.drawPixel(x, y, ST77XX_WHITE);
         }
     }
-    _display.display();
 }
